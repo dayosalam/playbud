b499c291bfed8c10679af5080b3da9600dca57e9
dayosalam <dayosalamboi@gmail.com>
2025-11-09 14:49:05 +0100

    Figured it out with owad

--- social-sport-app/vite.config.ts
+++ social-sport-app/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       "/api": {
-        target: "http://localhost:5173", // FastAPI
+        target: "http://localhost:2121", // FastAPI
         changeOrigin: true,
       },
     },
